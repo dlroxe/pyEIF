e47ce70b8b9e75c72a80536e541f7f6893d0d3b7
Su Wu <wusu2014@gmail.com>
2022-12-16 11:04:55 -0500

    add new functions to plot cnv distribution

--- Script/Fig1.py
+++ Script/Fig1.py
@@ -3,6 +3,7 @@ import lifelines
 import os
 import matplotlib
 import pandas
+import seaborn 
 import subprocess
 
 # critical parameter setting!
@@ -18,18 +19,111 @@ output_directory = "~/Documents/Bioinformatics_analysis/eIF4G-analysis/eIF4G_out
 # Call R.script to generate TCGA_CNV.csv data, perform heatmap clustering and pathway analysis #
 subprocess.call("Rscript /home/suwu/github/pyEIF/Script/Fig1.R", shell=True)
 
+TCGA_CNV = datatable.fread(os.path.join(data_file_directory, 
+"Gistic2_CopyNumber_Gistic2_all_thresholded.by_genes"), header=True).to_pandas()
+TCGA_CNV = TCGA_CNV.set_index('Sample')
+TCGA_CNV = TCGA_CNV.T
+TCGA_CNV = TCGA_CNV.replace([2], 'AMP')
+TCGA_CNV = TCGA_CNV.replace([1], 'DUP')
+TCGA_CNV = TCGA_CNV.replace([0], 'DIPLOID')
+TCGA_CNV = TCGA_CNV.replace([-1], 'DEL')
+TCGA_CNV = TCGA_CNV.replace([-2], 'HOMDEL')
+TCGA_CNV.head()
+
+count = TCGA_CNV.apply(pandas.value_counts, normalize=True).T
+
+def cnv_combined_freq_plot(df, cnv):
+    ax = matplotlib.pyplot.figure(figsize=(10, 10))
+    # Iterate through the five cnvs
+    for CNV in cnv:
+        # Subset to the airline
+        subset = df[CNV]
+        # Draw the density plot
+        ax = seaborn.kdeplot(subset, linewidth=2, fill=True, label = CNV) 
+    # Plot formatting
+    matplotlib.pyplot.legend(prop={'size': 16}, title = 'CNV')
+    matplotlib.pyplot.title('Density plot of CNV status in TCGA samples')
+    matplotlib.pyplot.xlabel('CNV frequency in all tumors')
+    matplotlib.pyplot.ylabel("Probability (%)")
+    matplotlib.pyplot.savefig(os.path.join(os.path.expanduser(output_directory), "Fig1", ("gain density plot.pdf")),
+                              dpi=300,
+                              edgecolor='w',
+                              #transparent=True,
+                              bbox_inches='tight') 
+    matplotlib.pyplot.show()
+
+cnv_combined_freq_plot(count, ['AMP', 'DUP'])
+cnv_combined_freq_plot(count, ['AMP', 'DUP', 'DIPLOID', 'DEL', 'HOMDEL'])
+
+
+##
+def cnv_freq_plot(df, cnv, cutoff):
+    ax = matplotlib.pyplot.figure(figsize=(10, 10))
+
+    # Draw the density curve with it's area shaded
+    seaborn.kdeplot(df[cnv], linewidth=2, fill=True)
+    # Invoke kdeplot() again to get reference to axes 
+    ax = seaborn.kdeplot(df[cnv])
+    
+    # Below code to shade partial region is from 
+    # https://stackoverflow.com/a/49100655
+    
+    # Get all the lines used to draw the density curve 
+    kde_lines = ax.get_lines()[-1]
+    kde_x, kde_y = kde_lines.get_data()
+    
+    # Use Numpy mask to filter the lines for region 
+    # reresenting height greater than 60 inches 
+    mask = kde_x > cutoff
+    filled_x, filled_y = kde_x[mask], kde_y[mask]
+    
+    # Shade the partial region 
+    ax.fill_between(filled_x, y1=filled_y)
+    
+    # find the number of genes over cutoff threshhold
+    number = len(df[df[cnv]>cutoff])
+    
+    ax.annotate(str(number)+' genes', xy=(0.06, 2), xytext=(0.07, 8),
+            arrowprops=dict(facecolor='black', shrink=0.05))
+    
+    # vertical line at x = 60 for reference
+    matplotlib.pyplot.axvline(x=cutoff, linewidth=2, linestyle='--')
+    matplotlib.pyplot.title("Density plot of "+str(cnv)+" status in TCGA samples")
+    matplotlib.pyplot.xlabel(str(cnv)+" frequency in all tumors")
+    matplotlib.pyplot.ylabel("Probability (%)")
+    matplotlib.pyplot.savefig(os.path.join(os.path.expanduser(output_directory), 
+                                           "Fig1", 
+                                           (cnv + "_density plot.pdf")),
+                              dpi=300,
+                              edgecolor='w',
+                              #transparent=True,
+                              bbox_inches='tight') 
+    matplotlib.pyplot.show()
+
+cnv_freq_plot(df=count, cnv="AMP", cutoff=0.05)
+
+
+
+
 ## KM survival analysis
-TCGA_OS = pandas.read_table(os.path.join(data_file_directory, 
-"Survival_SupplementalTable_S1_20171025_xena_sp")).set_index('sample')
+#TCGA_OS = pandas.read_table(os.path.join(data_file_directory, 
+#"Survival_SupplementalTable_S1_20171025_xena_sp")).set_index('sample')
+TCGA_OS = datatable.fread(os.path.join(data_file_directory, 
+"Survival_SupplementalTable_S1_20171025_xena_sp"), header=True).to_pandas()
+TCGA_OS = TCGA_OS.set_index('sample')
 TCGA_OS = TCGA_OS[["OS", "OS.time"]]
 
-TCGA_CNV = datatable.fread(os.path.join(os.path.expanduser(output_directory),"ProcessedData","TCGA_CNV.csv"), header=True).to_pandas()
-TCGA_CNV = TCGA_CNV.set_index(['C10'])
+
 TCGA_CNV_EIF = TCGA_CNV[["EIF4G1", "EIF3E", "EIF3H"]]
 
 
+#TCGA_CNV = datatable.fread(os.path.join(os.path.expanduser(output_directory),"ProcessedData","TCGA_CNV.csv"), header=True).to_pandas()
+#TCGA_CNV = TCGA_CNV.set_index(['C10'])
+#TCGA_CNV_EIF = TCGA_CNV[["EIF4G1", "EIF3E", "EIF3H"]]
+
+
 TCGA_CNV_OS_EIF = pandas.merge(TCGA_OS, 
-                               TCGA_CNV_EIF, 
+                               TCGA_CNV[["EIF4G1", "EIF3E", "EIF3H"]], 
                                left_index=True, 
                                right_index=True).dropna()
 
