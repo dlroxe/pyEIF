767ca4100bb82e8cefb03b741b7c20bc74f7ec0b
dlroxe <dlroxe@google.com>
2023-01-10 20:32:07 -0500

    Remove unnecessary code (second try).
    Eliminate the 'genedb' abstraction and the original 'hs_data' approach for Entrez lookups.  Leave a single initial unit test for 'org_es_hg_db' lookups.
    
    Also eliminate the initial attempt at reactome lookups.  Perhaps a sensible way may appear to make direct use of that database in Python some day, but today is not that day.

--- Script/input_data_adapters/genedb_lookup.py
+++
@@ -1,9 +0,0 @@
-"""This is a simple interface class for gene database lookups."""
-
-import abc
-
-
-class GeneDBLookup(abc.ABC):
-  @abc.abstractmethod
-  def translate_gene_symbol_to_entrez_id(self, gene_symbol: str) -> str:
-    pass  # only implemented by subclasses
---
+++ Script/input_data_adapters/org_hs_eg_db_lookup_tests.py
@@ -0,0 +1,12 @@
+"""Tests for hs_data_lookup.py."""
+
+from absl.testing import absltest
+
+import org_hs_eg_db_lookup
+
+
+class UnitTests(absltest.TestCase):
+
+  def test_degenerate_case(self):
+    handle = org_hs_eg_db_lookup.OrgHsEgDbLookup(org_hs_eg_db_file=None)
+    self.assertIsNone(handle.translate_gene_symbol_to_entrez_id('foo'))
