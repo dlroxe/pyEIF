d096305e9bb000fad1e0f386799a0b3ebb9f41ca
dlroxe <dlroxe@google.com>
2023-01-14 23:59:24 -0500

    Eliminate bogus trailing commas in threshold-01.csv (they wrongly specified an extra column), and eliminate all remaining manipulations of internal class data from tcga_cnv_parser_tests.py.

--- Script/input_data_adapters/tcga_cnv_parser_tests.py
+++ Script/input_data_adapters/tcga_cnv_parser_tests.py
@@ -1,9 +1,7 @@
 """Tests for init_data.py."""
 
-from absl import logging
 from absl.testing import absltest
 
-import os
 import org_hs_eg_db_lookup
 import tcga_cnv_parser
 import pandas
@@ -11,27 +9,16 @@ import pandas
 
 class UnitTests(absltest.TestCase):
   def test_threshold_label_substitution(self):
-    raw_threshold_data = pandas.DataFrame({
-      'Sample': ['gene1', 'gene2', 'gene3', 'gene4', 'gene5'],
-      'TCGA-A5-A0GI-01': [2.0, 0.0, 0.0, 0.0, 1.0],
-      'TCGA-S9-A7J2-01': [-2.0, -1.0, -1.0, -1.0, -1.0],
-      'TCGA-06-0150-01': [0.0, 0.0, 0.0, 0.0, 0.0],
-    }).set_index('Sample')
-
     expected_threshold_data = pandas.DataFrame({
       'Sample': ['gene1', 'gene2', 'gene3', 'gene4', 'gene5'],
       'TCGA-A5-A0GI-01': ['AMP', 'DIPLOID', 'DIPLOID', 'DIPLOID', 'DUP'],
       'TCGA-S9-A7J2-01': ['HOMDEL', 'DEL', 'DEL', 'DEL', 'DEL'],
       'TCGA-06-0150-01':
         ['DIPLOID', 'DIPLOID', 'DIPLOID', 'DIPLOID', 'DIPLOID'],
-    }).set_index('Sample').astype('category')
+    }).set_index('Sample').transpose().astype('category')
 
-    # TODO(dlroxe): It would be better to write data files and read them than
-    #               to set _threshold_raw_data and _threshold_data, or to call
-    #               _init_threshold_data().
-    parser = tcga_cnv_parser.TcgaCnvParser(None, None, None, None, None)
-    parser._threshold_raw_data = raw_threshold_data
-    parser._threshold_data = parser._init_threshold_data()
+    parser = tcga_cnv_parser.TcgaCnvParser('./input_data_adapters/test_data',
+                                           None, 'threshold-01.csv', None, None)
     all_threshold_data = parser.get_tcga_cnv_threshold_categories()
     self.assertTrue(expected_threshold_data.equals(all_threshold_data),
                     msg=expected_threshold_data.compare(all_threshold_data))
@@ -63,26 +50,12 @@ class UnitTests(absltest.TestCase):
       'phenotype-01.csv')
     joined_data = parser.merge_cnv_thresholds_and_phenotypes()
 
-    logging.info('joined data types:\n%s', joined_data.dtypes)
-    logging.info('expected data types:\n%s', expected_joined_data.dtypes)
-
     self.assertTrue(joined_data.equals(expected_joined_data),
                     msg='\n' + joined_data.compare(expected_joined_data))
 
   def test_top_genes(self):
-    threshold_data = pandas.DataFrame({
-      'Sample': ['gene1', 'gene2', 'gene3', 'gene4', 'gene5'],
-      'TCGA-A5-A0GI-01': ['AMP', 'DIPLOID', 'DIPLOID', 'DIPLOID', 'DUP'],
-      'TCGA-S9-A7J2-01': ['HOMDEL', 'DEL', 'DEL', 'DEL', 'DEL'],
-      'TCGA-06-0150-01': ['DIPLOID', 'DIPLOID', 'DIPLOID', 'DIPLOID',
-                          'DIPLOID'],
-    }).set_index('Sample').transpose()
-
-    sample_count = len(threshold_data.index)
-    # TODO(dlroxe): Reconstruct tests to avoid calling class internals.
-    parser = tcga_cnv_parser.TcgaCnvParser(None, None, None, None, None)
-    parser._threshold_data = threshold_data
-    parser._melted_threshold_data = parser._init_melted_threshold_data()
+    parser = tcga_cnv_parser.TcgaCnvParser(
+      './input_data_adapters/test_data', None, 'threshold-01.csv', None, None)
 
     genedb_handle = org_hs_eg_db_lookup.OrgHsEgDbLookup(org_hs_eg_db_file=None)
     top_genes = parser.get_top_genes  # for convenience
